8130826ca0810a129274c0acfecb173e4e2968bf
hubciodx2 <hubertsiast@gmail.com>
2018-12-27 14:19:42 +0100

    Poprawa walicacji, ulepszenie formularza od dowadania użytkowników

--- RunShawn.Web/App_Start/BundleConfig.cs
+++ RunShawn.Web/App_Start/BundleConfig.cs
@@ -11,8 +11,9 @@ namespace RunShawn.Web
                         "~/Scripts/jquery-{version}.js",
                         "~/Scripts/sidebar.js"));
 
-            bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
-                        "~/Scripts/jquery.validate*"));
+            bundles.Add(new ScriptBundle("~/bundles/validate").Include(
+                        "~/Scripts/validate/jquery.validate*",
+                        "~/Scripts/validate/bootstrap-validate.js"));
 
             // Use the development version of Modernizr to develop with and learn from. Then, when you're
             // ready for production, use the build tool at https://modernizr.com to pick only the tests you need.
@@ -22,22 +23,18 @@ namespace RunShawn.Web
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
                       "~/Scripts/bootstrap.js",
                       "~/Scripts/tooltip/jquery.darktooltip.js",
-                      "~/Scripts/tooltip/tooltipCustom.js"
-                      ));
+                      "~/Scripts/tooltip/tooltipCustom.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.min.css",
                       "~/Content/tooltip/darktooltip.css",
-                      "~/Content/site.css"));
+                      "~/Content/site/site.css"));
 
             bundles.Add(new StyleBundle("~/Content/dashboard").Include(
                       "~/Content/dashboard/dashboard.css"));
 
             bundles.Add(new StyleBundle("~/Content/css/login").Include(
-                      "~/Content/bootstrap.min.css",
                       "~/Content/login/login_form.css"));
-
-
         }
     }
 }
