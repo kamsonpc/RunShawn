753d044f91d7101e88cf610d194ed276db97aeb5
B.Kamil <kamilbaczek@gmail.com>
2019-01-06 18:32:01 +0100

    Naprawienie buga z linkami do frontu

---
+++ RunShawn.Web/Areas/Default/DefaultAreaRegistration.cs
@@ -0,0 +1,19 @@
+﻿using System.Web.Mvc;
+
+namespace RunShawn.Web.Areas.Admin
+{
+    public class DefaultAreaRegistration : AreaRegistration
+    {
+        public override string AreaName => "Default";
+
+        public override void RegisterArea(AreaRegistrationContext context)
+        {
+            context.MapRoute(
+                "Default_default",
+                "{controller}/{action}/{id}",
+                new { controller = "News", action = "Index", id = UrlParameter.Optional },
+                namespaces: new[] { "RunShawn.Web.Areas.Default.Controllers" }
+            );
+        }
+    }
+}
