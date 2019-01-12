a49dccb1f94a59861341a150d26fd14486d89cc3
Kamil <Kamil@DESKTOP-FOUK3EL>
2019-01-12 20:27:05 +0100

    Strony dodanie Routingu

--- RunShawn.Web/App_Start/RouteConfig.cs
+++ RunShawn.Web/App_Start/RouteConfig.cs
@@ -8,7 +8,7 @@ namespace RunShawn.Web
         public static void RegisterRoutes(RouteCollection routes)
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
-
+            routes.MapMvcAttributeRoutes();
             AreaRegistration.RegisterAllAreas();
             routes.MapRoute(
                 name: "Client",
