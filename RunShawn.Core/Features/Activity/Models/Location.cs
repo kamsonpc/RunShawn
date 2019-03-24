00689cd8b0391bf3dcc955d74f73de1b81ee3157
Kamil <Kamil@DESKTOP-FOUK3EL>
2019-03-24 05:19:47 +0100

    Doładowanie punktów

---
+++ RunShawn.Core/Features/Activity/Models/Location.cs
@@ -0,0 +1,11 @@
+﻿namespace RunShawn.Core.Features.Activity.Models
+{
+    public class LocationObject
+    {
+        public long Id { get; set; }
+
+        public string Location { get; set; }
+
+        public long Boost { get; set; }
+    }
+}
